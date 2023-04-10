1f91eef17d14501b2974b44e89ba004d53058ce9
Guy1m0 <root@Jingchis-MacBook-Pro.local>
2023-04-10 21:40:25 +0800

    Add a demo video

--- examples/auction/UI/script.js
+++ examples/auction/UI/script.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     displayAuctions();
   } else if (window.location.pathname.includes("view_auction.html")) {
     loadAuction();
+    setEndAuctionBtnState();
   }
+  //updateBidderAddressDisplay();
+
   /*
   if (window.location.pathname.includes("login.html")) {
     alert("login page");
@@ -28,7 +31,7 @@ document.getElementById("client-option").addEventListener("change", function () 
   const auctionId = new URLSearchParams(window.location.search).get("id");
   const auctions = JSON.parse(localStorage.getItem("auctions")) || [];
   const auction = auctions.find((auction) => auction.id === parseInt(auctionId));
-  window.alert(auction.assetId);
+  //window.alert(auction.assetId);
 
 
   if (selectedOption === "eth") {
@@ -40,16 +43,30 @@ document.getElementById("client-option").addEventListener("change", function () 
   }
 });
 
+
 let bidderAddress = '';
+let filename = '';
+
+function updateBidderAddressDisplay() {
+  const contents = localStorage.getItem("fileContents");
+  if (contents){
+    const json = JSON.parse(contents);
+  
+    bidderAddress = json.address;
+    window.alert(bidderAddress);
+    const shortenedBidderAddress = bidderAddress.slice(0, 5) + '...' + bidderAddress.slice(-5);
+    document.getElementById("bidder-address").textContent = "0x" + shortenedBidderAddress;
+
+  }
+}
 
 //addAsset("asset1")
   function resetAuctions() {
-    localStorage.removeItem('auctions');
-    auctions = [];
+    localStorage.clear()
     displayAuctions();
   }
   
-  function createAuction() {
+  async function createAuction() {
     //window.alert("create Auction");
     const assetId = document.getElementById("asset-id").value;
 
@@ -57,41 +74,45 @@ let bidderAddress = '';
     //const blockType = document.getElementById("blockchain-type").value;
     //const nftType = document.getElementById("nft-type").value;
     //const blockAddress = document.getElementById("blockchain-address").value;
-    const conclusionTime = document.getElementById("conclusion-time").value;
+    const conclusionTime = new Date(document.getElementById("conclusion-time").value).getTime();
     const description = document.getElementById("auction-description").value;
     const checkedBoxes = document.querySelectorAll('.sup-BC:checked');
     const supportBlock = Array.from(checkedBoxes).map(checkbox => checkbox.value);
 
-    //const response = startAuction(assetId);
+    const response = await startAuction(assetId);
     //const data = await response.json();
-    //window.alert(response.data.owner);
+    //window.alert(response.Owner);
 
     let auctions = JSON.parse(localStorage.getItem("auctions")) || [];  
     const auction = {
-      id: auctions.length + 1,
+      id: response.ID,
       assetId: assetId,
-      conclusionTime: new Date(conclusionTime).getTime(),
+      conclusionTime: conclusionTime,
       description: description,
       supportBlock: supportBlock,
-      //EthAddr: response.Data.EthAddr,
-      //QuorumAddr: response.Data.QuorumAddr,
+      EthAddr: response.Eth,
+      QuorumAddr: response.Quorum,
+      //HiddenID: response.ID,
       topBid: 0,
       bids: [],
     };
   
+    //window.alert("Hidden ID:"+response.ID);
     auctions.push(auction);
     localStorage.setItem("auctions", JSON.stringify(auctions));
   
 
-    const response = addAsset(assetId);
-    window.alert("Asset added by " + response.Owner);
+    //addAsset(assetId);
+    //window.alert("Asset added by " + response.Owner);
+    
     window.location.href = "index.html";
-
+    
     //window.alert("Adding asset?");
 
     displayAuctions();
+    //document.getElementById("response").textContent = "return" + response.Owner;
 
-
+    //window.alert(response.Owner);
     // Testing addAsset
 
   }
@@ -101,18 +122,26 @@ let bidderAddress = '';
     const file = fileInput.files[0];
     const reader = new FileReader();
   
+    //window.alert("load?");
     reader.onload = function (event) {
       const contents = event.target.result;
       const json = JSON.parse(contents);
   
       bidderAddress = json.address;
-      document.getElementById("bidder-address").textContent = "0x" + bidderAddress;
+      //window.alert(bidderAddress);
+      const shortenedBidderAddress = bidderAddress.slice(0, 5) + '...' + bidderAddress.slice(-5);
+      document.getElementById("bidder-address").textContent = "0x" + shortenedBidderAddress;
+      filename = file.name;
+
+      //localStorage.setItem("fileContents", contents)
+      //updateBidderAddressDisplay();
+
     };
   
     reader.readAsText(file);
   }
 
-  function addBid() {
+  async function addBid() {
     //window.alert("Adding bid");
     const auctionId = new URLSearchParams(window.location.search).get("id");
     const auctions = JSON.parse(localStorage.getItem("auctions")) || [];
@@ -121,17 +150,48 @@ let bidderAddress = '';
     //
     const bidder = "0x" + bidderAddress;
     const amount = parseInt(document.getElementById('bid-value').value,10);
+    const client = document.getElementById("client-option").value;
+
 
     if (!bidder || !amount) {
       alert('Please load key file and fill in Bid Amount fields.');
       return;
     }
+
     const bid = {
       id: auction.bids.length + 1,
       bidder: bidder,
       amount: amount,
-      timestamp: new Date().toISOString().replace('T', ' ').substr(0, 19)
+      timestamp: new Date().toLocaleString()
     };
+
+    AucAddr = ""
+    if (client === "eth") {
+      AucAddr = auction.EthAddr;
+      bid.platform = "Ethereum";
+    } else if (client === "quo") {
+      AucAddr = auction.QuorumAddr;
+      bid.platform = "Quorum"
+    } else {
+      AucAddr = "";
+      bid.platform = ""
+    }
+
+    
+    try {
+      // Call backend API
+      const response = await fetch(`http://localhost:6789/api/add-bid?address=${AucAddr}&platform=${client}&filename=${filename}&amount=${amount}`);
+      const data = await response.json();
+      //window.alert(data.Status);
+      // Handle response from the backend API
+    } catch (error) {
+      //window.alert(error);
+      console.error("Error calling backend API:", error);
+    }
+    
+
+
+    //new Date(conclusionTime).toLocaleString()
 
     //localStorage.setItem(auctionId, JSON.stringify(auction));
     //window.alert(auction.topBid+"?"+bid.amount);
@@ -142,12 +202,11 @@ let bidderAddress = '';
     //auctions[index].topBid = bid.amount;
     auctions[index].bids.push(bid);
     localStorage.setItem("auctions", JSON.stringify(auctions));
-    document.getElementById("top-bid").textContent = auction.topBid;
+    document.getElementById("top-bid").textContent ="$"+ auction.topBid;
 
     displayBid(bid);
     //document.getElementById("top-bid").textContent = auction.bids.length > 0 ? amount : "N/A";
   }
-  
   
   function displayAuctions() {
     const auctionsContainer = document.getElementById("auction-list");
@@ -165,7 +224,7 @@ let bidderAddress = '';
           <div class="auction-item" data-active=${isActive}>
             <h4>Auction #${auction.id}</h4>
             <p>Number of Bids: ${auction.bids.length}</p>
-            <p>Top Bid: ${auction.topBid}</p>
+            <p>Top Bid: $${auction.topBid}</p>
             <p>Active: ${isActive}</p>
             <a href="view_auction.html?id=${auction.id}"><button class="view-btn" id="view-auction-page">View</button></a>
           </div>
@@ -174,8 +233,6 @@ let bidderAddress = '';
     });
 
     auctionsContainer.innerHTML = auctionsHtml;
-
-
   }
   
   function loadAuction() {
@@ -197,14 +254,16 @@ let bidderAddress = '';
     //document.getElementById("nft-type").textContent = auction.nftType;
     //document.getElementById("blockchain-address").textContent = auction.blockAddress;
     document.getElementById("auction-description").textContent = auction.description;
-    document.getElementById("top-bid").textContent = auction.topBid;
+    document.getElementById("top-bid").textContent = "$" + auction.topBid;
 
     document.getElementById("conclusion-time").textContent = new Date(auction.conclusionTime).toLocaleString();
     const isActive = Date.now() < auction.conclusionTime ? "true" : "false";
     document.getElementById("active-status").textContent = isActive;
 
     const bidSection = document.getElementById("bid-section");
+    const endBtn = document.getElementById("end-auction-btn");
     bidSection.style.display = isActive === "true" ? "block" : "none";
+    endBtn.style.display = isActive === "false" ? "block" : "none";
     
     for (const bid of auction.bids) {
       displayBid(bid);
@@ -221,14 +280,43 @@ let bidderAddress = '';
     */
   }
 
+  function endAuctionOnClick() {
+    //window.alert("Ending Auction");
+    const auctionId = new URLSearchParams(window.location.search).get("id");
+    const auctions = JSON.parse(localStorage.getItem("auctions")) || [];
+    const auction = auctions.find((auction) => auction.id === parseInt(auctionId));
+
+    const assetID = auction.assetId;
+ 
+    endAuction(assetID, auctionId);
+    const endAuctionBtn = document.getElementById("end-auction-btn");
+    endAuctionBtn.setAttribute("disabled", "disabled");
+    localStorage.setItem(`endAuctionBtnDisabled_${auctionId}`, true);
+  }
+  
+  function setEndAuctionBtnState() {
+    const auctionId = new URLSearchParams(window.location.search).get("id");
+    const endAuctionBtn = document.getElementById("end-auction-btn");
+    const isBtnDisabled = localStorage.getItem(`endAuctionBtnDisabled_${auctionId}`);
+
+  
+    if (isBtnDisabled === "true") {
+      endAuctionBtn.setAttribute("disabled", "disabled");
+    } else {
+      endAuctionBtn.removeAttribute("disabled");
+    }
+  }
+  
+
   function displayBid(bid) {
     const bidElement = document.createElement('div');
     bidElement.className = 'bid';
   
     bidElement.innerHTML = `
       <p>Bid #${bid.id}</p>
-      <p>Bidder: ${bid.bidder}</p>
-      <p>Amount: ${bid.amount} ETH</p>
+      <p>Bidder Platfrom: ${bid.platform} </p>
+      <p>Address: ${bid.bidder}</p>
+      <p>Amount: $${bid.amount}</p>
       <p>Timestamp: ${bid.timestamp}</p>
     `;
   
@@ -243,68 +331,48 @@ let bidderAddress = '';
 
 async function addAsset(assetID) {
   //window.alert("Try add asset");
-  try {
+  try {    
       const response = await fetch(`http://localhost:6789/api/add-asset?asset_id=${assetID}`);
 
       console.log("Response object:", response);
       if (!response.ok) {
         throw new Error(`HTTP error ${response.status}`);
       }
+
       const data = await response.json();
-      window.alert("Asset added:"+data.Owner);
+      //console.log("Asset added:"+data);
       return data;
       //window.alert(data.owner);
   } catch (error) {
-      window.alert(error);
+      //window.alert(error);
       console.error("Error adding asset:", error);
   }
 }
-
 
 async function startAuction(assetID) {
   //window.alert("Start Auction?");
   try {
       const response = await fetch(`http://localhost:6789/api/start-auction?asset_id=${assetID}`);
       const data = await response.json();
-      window.alert("startAuction: "+data.EthAddr);
+      //window.alert("start Auction on address: "+data.Quorum);
       return data;
 
   } catch (error) {
-      window.alert(error);
+      //window.alert(error);
       console.error("Error adding asset:", error);
   }
 }
 
-/*
-async function startAuction(assetID) {
-  const TIMEOUT_MS = 10000; // 10 seconds
-  const controller = new AbortController();
-  const { signal } = controller;
-  setTimeout(() => controller.abort(), TIMEOUT_MS);
-
+async function endAuction(assetID, auctionID) {
   try {
-    const response = await fetch(`http://localhost:6789/api/start-auction?asset_id=${assetID}`, {
-      method: 'GET',
-      signal,
-    });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
+    const response = await fetch(`http://localhost:6789/api/end-auction?asset_id=${assetID}&auction_id=${auctionID}`);
     const data = await response.json();
-    window.alert("startAuction: " + data.EthAddr);
+    //console.log(data.message);
     return data;
   } catch (error) {
-    if (error.name === 'AbortError') {
-      window.alert('Request timed out');
-    } else {
-      window.alert("other error:"+error);
-      console.error("Error adding asset:", error);
-    }
+    console.error("Error ending auction:", error);
   }
 }
 
-*/
 
 
